99f9c45b832634c5e8fb6e6efa0b3c2db92d182a
bayapucg <vasudevareddy549@gmail.com>
2018-04-07 11:02:56 +0530

    commit
    changtes

--- application/views/prescription/prescription_view.php
+++ application/views/prescription/prescription_view.php
@@ -14,56 +14,96 @@
 			  </div>
 		   </div>
 					<div class="row">
-                       <div class="col-md-12">
-                            <div class="card card-topline-aqua">
-							 <?php if($this->session->flashdata('success')): ?>
-										<div class="alert_msg1 animated slideInUp bg-succ">
-										<?php echo $this->session->flashdata('success');?> &nbsp; <i class="glyphicon glyphicon-ok text-success ico_bac" aria-hidden="true"></i>
-										</div>
-									<?php endif; ?>
-									<?php if($this->session->flashdata('error')): ?>
-										<div class="alert_msg1 animated slideInUp bg-warn">
-										<?php echo $this->session->flashdata('error');?> &nbsp; <i class="glyphicon glyphicon-ok text-success ico_bac" aria-hidden="true"></i>
-										</div>
-									<?php endif; ?>
-                                <div class="card-head">
-                                     <header>Prescription  List</header>
-                                   
+                                <div class="col-md-12">
+                                    <div class="card ">
+                                        <div class="card-head">
+                                            <header>Prescription List </header>
+                                          
+                                        </div>
+                                        <div class="card-body " style="padding: 0px 24px 24px 24px;">
+                                        <div class="table-responsive">
+										<form>
+                                            <table class="table custom-table table-hover" style="border-top:none">
+                                                <thead >
+                                                    <tr >
+                                                        <th> S.NO</th>
+                                                        <th>Medicine Name</th>
+                                                        <th>QTY</th>
+                                                        <th>Dosage</th>
+                                                        <th>Usage </th>
+                                                        <th>Usage Instructions</th>
+                                                        <th>Substitute allowed?</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>1</td>
+                                                        <td>Medicine Name</td>
+                                                        <td style="width:100px">
+															<div class="form-group">
+															<input type="text" class="form-control" placeholder="Enter Qty">
+															</div>
+														</td>
+                                                        <td>
+															600
+														</td>
+                                                        <td>
+															<div class="form-group">
+																
+																<select class="form-control">
+																	<option>option 1</option>
+																	<option>option 2</option>
+																	<option>option 3</option>
+																
+																</select>
+															</div>
+														</td>
+                                                        <td>Lorem Ipsum dorolo imit</td>
+                                                        <td>Lorem Ipsum dorolo imit</td>
+                                                       
+                                                    </tr>
+													<tr>
+                                                        <td>2</td>
+                                                        <td>Medicine Name</td>
+                                                        <td style="width:100px">
+															<div class="form-group">
+															<input type="text" class="form-control" placeholder="Enter Qty">
+															</div>
+														</td>
+                                                        <td>
+															600
+														</td>
+                                                        <td>
+															<div class="form-group">
+																
+																<select class="form-control">
+																	<option>option 1</option>
+																	<option>option 2</option>
+																	<option>option 3</option>
+																
+																</select>
+															</div>
+														</td>
+                                                        <td>Lorem Ipsum dorolo imit</td>
+                                                        <td>Lorem Ipsum dorolo imit</td>
+                                                       
+                                                    </tr>
+                                                    
+                                                   
+                                                    
+                                                </tbody>
+                                            </table>
+											<div class="pull-right">
+											<button class="btn btn-warning">Print Prescription</button>
+											<button class="btn btn-success">Print Prescription</button>
+											</div>
+
+											</form>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="card-body ">
-								<?php if(count($prescriptions)>0){ ?>
-                                    <table id="saveStage" class="display" style="width:100%;">
-                                        <thead>
-                                            <tr>
-												<th>Patient Id</th>
-												<th>Name</th>
-                                                <th>Referred By</th>
-                                                <th>Date</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-										<?php foreach($prescriptions as $list){ ?>
-                                            <tr>
-                                                <td><?php echo htmlentities($list['pid']); ?></td>
-                                                <td><?php echo htmlentities($list['name']); ?></td>
-                                                <td><?php echo htmlentities($list['created_by']); ?></td>
-                                                <td><?php echo date('M j Y h:i A',strtotime(htmlentities($list['create_at'])));?></td>
-                                                <td><a href="<?php echo base_url('users/prescriptionview/'.base64_encode($list['pid']).'/'.base64_encode($list['b_id'])); ?>">view</a></td>
-                                               
-                                            </tr>
-										<?php } ?>
-											
-                                            
-                                        </tbody>
-                                    </table>
-								<?php } ?>
-                                </div>
-								<div class="clearfix">&nbsp;</div>
-							
                             </div>
-                        </div>
-                    </div>
 				
                     
                 </div>
