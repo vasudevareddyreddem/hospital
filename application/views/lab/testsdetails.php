c52bbdc1cfbc8d9122552890aa87a730f7b1d96c
vasudevareddyreddem <vasudevareddy549@gmail.com>
2018-06-13 15:09:18 +0530

    pop up  adding

--- application/views/lab/testsdetails.php
+++ application/views/lab/testsdetails.php
@@ -110,11 +110,29 @@
                                                 <td><?php echo htmlentities($list['t_short_form']); ?></td>
                                                 <td><?php echo htmlentities($list['t_description']); ?></td>
 												<td><?php if($list['status']==1){ echo "Active";}else{ echo "Deactive"; } ?></td>
-												<td><a href="<?php echo base_url('lab/teststatus/'.base64_encode($list['t_id']).'/'.base64_encode($list['status'])); ?>">
-                                                         <?php if($list['status']==0){ echo "Active";}else{  echo "Deactive";}?>  </a> |
-												<a href="<?php echo base_url('lab/edit/'.base64_encode($list['t_id'])); ?>">Edit</a> |
-												<a href="<?php echo base_url('lab/deletelab/'.base64_encode($list['t_id'])); ?>">Delete</a>
-                                                </td>                                               
+												
+												<td class="valigntop">
+                                                    <div class="btn-group">
+                                                        <button class="btn btn-xs deepPink-bgcolor dropdown-toggle no-margin" type="button" data-toggle="dropdown" aria-expanded="false"> Actions
+                                                            <i class="fa fa-angle-down"></i>
+                                                        </button>
+                                                        <ul class="dropdown-menu" role="menu">
+                                                            <li>
+                                                                <a href="<?php echo base_url('lab/edit/'.base64_encode($list['t_id'])); ?>">
+                                                                    <i class="fa fa-edit"></i>EDit </a>
+                                                            </li>
+                                                            <li>
+                                                                <a href="<?php echo base_url('lab/deletelab/'.base64_encode($list['t_id'])); ?>">
+                                                                    <i class="fa fa-trash-o"></i>Delete</a>
+                                                            </li>
+															<li>
+                                                                <a href="javascript;void(0);" onclick="admindeactive('<?php echo base64_encode(htmlentities($list['t_id'])).'/'.base64_encode(htmlentities($list['status']));?>');adminstatus('<?php echo $list['status'];?>')" href="javascript:void(0)" data-toggle="modal" data-target="#myModal">
+                                                                    <i class="fa fa-edit"></i><?php if($list['status']==0){ echo "Active";}else{ echo "Deactive"; } ?> </a>
+                                                            </li>
+                                                            
+                                                        </ul>
+                                                    </div>
+                                                </td>												
                                             </tr>
 										<?php } ?>
 											
@@ -131,11 +149,50 @@
                </div>
             </div>
             <div class="clearfix">&nbsp;</div>
-       
+       <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+			
+			<div style="padding:10px">
+			<button type="button" class="close" data-dismiss="modal">&times;</button>
+			<h4 style="pull-left" class="modal-title">Confirmation</h4>
+			</div>
+			<div class="modal-body">
+			<div class="alert alert-danger alert-dismissible" id="errormsg" style="display:none;"></div>
+			  <div class="row">
+				<div id="content1" class="col-xs-12 col-xl-12 form-group">
+				Are you sure ? 
+				</div>
+				<div class="col-xs-6 col-md-6">
+				  <button type="button" aria-label="Close" data-dismiss="modal" class="btn  blueBtn">Cancel</button>
+				</div>
+				<div class="col-xs-6 col-md-6">
+                <a href="?id=value" class="btn  blueBtn popid" style="text-decoration:none;float:right;"> <span aria-hidden="true">Ok</span></a>
+				</div>
+			 </div>
+		  </div>
+      </div>
+      
+    </div>
+  </div>
       </div>
    </div>
 </div>
 <script>
+ function admindeactive(id){
+	$(".popid").attr("href","<?php echo base_url('lab/teststatus/'); ?>"+"/"+id);
+}
+function adminstatus(id){
+	if(id==1){
+			$('#content1').html('Are you sure you want to Deactivate?');
+		
+	}if(id==0){
+			$('#content1').html('Are you sure you want to activate?');
+	}
+}
+  
 $(document).ready(function() {
     $('#addtreatment').bootstrapValidator({
         
