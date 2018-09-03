c23dc22d3bf6b79b9f5aeaa48fa046ca3d210ad2
banuribhavya <banuribhavya@users.github.com>
2018-09-03 13:27:18 +0530

    update

--- hospital-home-page/register_form.php
+++ hospital-home-page/register_form.php
@@ -13,19 +13,16 @@ if(isset($_POST['form_one']) && $_POST['form_one']==1){
 	$sql="INSERT INTO register(h_name,r_name,mobilenumber,email,message,status)
 	VALUES  ('".$_POST['h_name']."','".$_POST['r_name']."','".$_POST['mobilenumber']."','".$_POST['email']."','".$_POST['message']."','0')";
 	//echo $sql;exit;
-	$to = "medspaceit@gmail.com";
-	$h_name = $_POST['h_name'];
-	$r_name = $_POST['r_name'];
-	$mobilenumber=$_POST['mobilenumber'];
+	$to = "medspaceit@gmail.com";	
 	$from = $_POST['email'];
 	$subject = "Form submission";
-	$message = $r_name ." wrote the following:" . "\n\n" . $_POST['message'];
+	$message =$_POST['r_name']." wrote the following:" . "\n\n" ."Name of the Hospital:".$_POST['h_name']."\n\n"."Name of the representative:".$_POST['r_name']."\n\n"."Mobile Number:".$_POST['mobilenumber']."\n\n"."Message:".$_POST['message'];
 	$headers = "From:" . $from;
 	$currentPath = $_SERVER['PHP_SELF']; 
 	$pathInfo = pathinfo($currentPath); 
 	//echo $pathInfo['dirname'];exit;
 	if($conn->query($sql) === TRUE) {
-		if(mail($to,$h_name,$r_name,$mobilenumber,$subject,$message,$headers)==true){			
+		if(mail($to,$subject,$message,$headers)==true){			
 			$msg='success';
 			$id=$conn->insert_id;
 			$sql1='UPDATE contact SET status=1 WHERE id='.$id;
