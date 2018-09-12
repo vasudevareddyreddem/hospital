1d65a75f851ff29e418632d9a46173bd9b893ad4
SathwikGunda <42731745+SathwikGunda@users.noreply.github.com>
2018-09-12 16:04:36 +0530

    commit

---
+++ application/views/seller-admin/header.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+ 
+    <title>hosp</title>
+    <!-- google font -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&amp;subset=all" rel="stylesheet" type="text/css" />
+	<!-- icons -->
+    <link href="assets/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<!--bootstrap -->
+    <link href="assets/tether/css/tether.min.css" rel="stylesheet" type="text/css" />
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="css/bootstrapValidator.min.css" rel="stylesheet" type="text/css" />
+    <!-- Material Design Lite CSS -->
+	 <link href="assets/datatables/plugins/bootstrap/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css"/>
+	<link rel="stylesheet" href="assets/material/material.min.css">
+	<link rel="stylesheet" href="css/material_style.css">
+	<!-- Theme Styles -->
+    <link href="css/theme_style.css" rel="stylesheet" id="rt_style_components" type="text/css" />
+    <link href="css/plugins.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/custom.css" rel="stylesheet" type="text/css" />
+    <link href="css/responsive.css" rel="stylesheet" type="text/css" />
+    <link href="css/theme-color.css" rel="stylesheet" type="text/css" />
+	<link href="assets/select2/css/select2.css" rel="stylesheet" type="text/css" />
+    <link href="assets/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+	  <script src="assets/jquery.min.js" ></script>
+    
+ </head>
+ <!-- END HEAD -->
+<body class="page-header-fixed sidemenu-closed-hidelogo page-content-white page-md header-indigo white-sidebar-color logo-indigo">
+    <div class="page-wrapper">
+        <!-- start header -->
+        <div class="page-header navbar navbar-fixed-top">
+            <div class="page-header-inner ">
+                <!-- logo start -->
+                <div class="page-logo">
+                    <a href="dashboard.php">
+                    
+                    <span class="logo-default" >Seller Admin</span> </a>
+                </div>
+                <!-- logo end -->
+				<ul class="nav navbar-nav navbar-left in">
+					<li><a href="#" class="menu-toggler sidebar-toggler"><i class="icon-menu"></i></a></li>
+				</ul>
+                 <form class="search-form-opened" action="#" method="GET">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search..." name="query">
+                        <span class="input-group-btn">
+                          <a href="javascript:;" class="btn submit">
+                             <i class="icon-magnifier"></i>
+                           </a>
+                        </span>
+                    </div>
+                </form>
+                <!-- start mobile menu -->
+                <a href="javascript:;" class="menu-toggler responsive-toggler" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span></span>
+                </a>
+               <!-- end mobile menu -->
+                <!-- start header menu -->
+                <div class="top-menu">
+                    <ul class="nav navbar-nav pull-right">
+                    	<!-- start language menu -->
+                       
+                        <!-- end language menu -->
+                        <!-- start notification dropdown -->
+                        <li class="dropdown dropdown-extended dropdown-notification" id="header_notification_bar">
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                                <i class="fa fa-bell-o"></i>
+                                <span class="badge headerBadgeColor1"> 6 </span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="external">
+                                    <h3><span class="bold">Notifications</span></h3>
+                                    <span class="notification-label purple-bgcolor">New 6</span>
+                                </li>
+                                <li>
+                                    <ul class="dropdown-menu-list small-slimscroll-style" data-handle-color="#637283">
+                                        <li>
+                                            <a href="javascript:;">
+                                                <span class="time">just now</span>
+                                                <span class="details">
+                                                <span class="notification-icon circle deepPink-bgcolor"><i class="fa fa-check"></i></span> Congratulations!. </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;">
+                                                <span class="time">3 mins</span>
+                                                <span class="details">
+                                                <span class="notification-icon circle purple-bgcolor"><i class="fa fa-user o"></i></span>
+                                                <b>John Micle </b>is now following you. </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;">
+                                                <span class="time">7 mins</span>
+                                                <span class="details">
+                                                <span class="notification-icon circle blue-bgcolor"><i class="fa fa-comments-o"></i></span>
+                                                <b>Sneha Jogi </b>sent you a message. </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;">
+                                                <span class="time">12 mins</span>
+                                                <span class="details">
+                                                <span class="notification-icon circle pink"><i class="fa fa-heart"></i></span>
+                                                <b>Ravi Patel </b>like your photo. </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;">
+                                                <span class="time">15 mins</span>
+                                                <span class="details">
+                                                <span class="notification-icon circle yellow"><i class="fa fa-warning"></i></span> Warning! </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:;">
+                                                <span class="time">10 hrs</span>
+                                                <span class="details">
+                                                <span class="notification-icon circle red"><i class="fa fa-times"></i></span> Application error. </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div class="dropdown-menu-footer">
+                                        <a href="javascript:void(0)"> All notifications </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- end notification dropdown -->
+                        <!-- start message dropdown -->
+ 						<li class="dropdown dropdown-extended dropdown-inbox" id="header_inbox_bar">
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                                <i class="fa fa-envelope-o"></i>
+                                <span class="badge headerBadgeColor2"> 2 </span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li class="external">
+                                    <h3><span class="bold">Messages</span></h3>
+                                    <span class="notification-label cyan-bgcolor">New 2</span>
+                                </li>
+                                <li>
+                                    <ul class="dropdown-menu-list small-slimscroll-style" data-handle-color="#637283">
+                                        <li>
+                                            <a href="#">
+                                                <span class="photo">
+                                                	<img src="img/doc/doc2.jpg" class="img-circle" alt=""> </span>
+                                                <span class="subject">
+                                                	<span class="from"> Sarah Smith </span>
+                                                	<span class="time">Just Now </span>
+                                                </span>
+                                                <span class="message"> Jatin I found you on LinkedIn... </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span class="photo">
+                                                	<img src="img/doc/doc3.jpg" class="img-circle" alt=""> </span>
+                                                <span class="subject">
+                                                	<span class="from"> John Deo </span>
+                                                	<span class="time">16 mins </span>
+                                                </span>
+                                                <span class="message"> Fwd: Important Notice Regarding Your Domain Name... </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span class="photo">
+                                                	<img src="img/doc/doc1.jpg" class="img-circle" alt=""> </span>
+                                                <span class="subject">
+                                                	<span class="from"> Rajesh </span>
+                                                	<span class="time">2 hrs </span>
+                                                </span>
+                                                <span class="message"> pls take a print of attachments. </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span class="photo">
+                                                	<img src="img/doc/doc8.jpg" class="img-circle" alt=""> </span>
+                                                <span class="subject">
+                                                	<span class="from"> Lina Smith </span>
+                                                	<span class="time">40 mins </span>
+                                                </span>
+                                                <span class="message"> Apply for Ortho Surgeon </span>
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a href="#">
+                                                <span class="photo">
+                                                	<img src="img/doc/doc5.jpg" class="img-circle" alt=""> </span>
+                                                <span class="subject">
+                                                	<span class="from"> Jacob Ryan </span>
+                                                	<span class="time">46 mins </span>
+                                                </span>
+                                                <span class="message"> Request for leave application. </span>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <div class="dropdown-menu-footer">
+                                        <a href="#"> All Messages </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- end message dropdown -->
+ 						<!-- start manage user dropdown -->
+ 						<li class="dropdown dropdown-user">
+                            <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">
+                                <img alt="" class="img-circle " src="img/dp.jpg" />
+                                <span class="username username-hide-on-mobile"> Admin </span>
+                                <i class="fa fa-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-default">
+                                <li>
+                                    <a href="user_profile.html">
+                                        <i class="icon-user"></i> Profile </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="icon-settings"></i> Settings
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="icon-directions"></i> Help
+                                    </a>
+                                </li>
+                                <li class="divider"> </li>
+                                <li>
+                                    <a href="lock_screen.html">
+                                        <i class="icon-lock"></i> Lock
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="login.html">
+                                        <i class="icon-logout"></i> Log Out </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- end manage user dropdown -->
+                        <li class="dropdown dropdown-quick-sidebar-toggler">
+                             <a id="headerSettingButton" class="mdl-button mdl-js-button mdl-button--icon pull-right" data-upgraded=",MaterialButton">
+	                           <i class="material-icons">more_vert</i>
+	                        </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+      
+    </div>
+   
---
+++ application/views/seller-pharmacy/footer.php
@@ -0,0 +1,36 @@
+
+    <!-- start js include path -->
+    <script src="assets/jquery.min.js" ></script>
+    <script src="assets/tether/js/tether.min.js" ></script>
+    <script src="assets/jquery.blockui.min.js" ></script>
+	<script src="assets/jquery.slimscroll.js"></script>
+    <!-- bootstrap -->
+    <script src="assets/bootstrap/js/bootstrap.min.js" ></script>
+    <script src="assets/bootstrapValidator.min.js" ></script>
+    <script src="assets/bootstrap-switch/js/bootstrap-switch.min.js" ></script>
+    <!-- counterup -->
+	<!-- data tables -->
+    <script src="assets/datatables/jquery.dataTables.min.js" ></script>
+ 	<script src="assets/datatables/plugins/bootstrap/dataTables.bootstrap4.min.js" ></script>
+    <script src="assets/table_data.js" ></script>
+    <!-- Common js-->
+    <script src="assets/counterup/jquery.waypoints.min.js" ></script>
+    <script src="assets/counterup/jquery.counterup.min.js" ></script>
+    <!-- Common js-->
+	<script src="assets/app.js" ></script>
+    <script src="assets/layout.js" ></script>
+    <script src="assets/theme-color.js" ></script>
+    <!-- material -->
+    <script src="assets/material/material.min.js"></script>
+	<script src="assets/chart-js/Chart.bundle.js" ></script>
+    <script src="assets/chart-js/utils.js" ></script>
+    <script src="assets/chart-js/home-data2.js" ></script>
+    <script src="assets/sparkline/jquery.sparkline.js" ></script>
+	<script src="assets/sparkline/sparkline-data.js" ></script>
+	<script src="assets/select2/js/select2.js" ></script>
+    <script src="assets/select2/js/select2-init.js" ></script>
+    
+
+  </body>
+
+</html>
